0b46bdd0efb1ebf5e2b709178b320e7fe2628694
Diogo Silva <dioghx33@gmail.com>
2025-11-17 22:49:25 -0300

    Adicionado remessaProducer

---
+++ remessa-producer/src/main/java/com/floorplan/remessaproducer/RemessaProducerApplication.java
@@ -0,0 +1,13 @@
+package com.floorplan.remessaproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemessaProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemessaProducerApplication.class, args);
+	}
+
+}
---
+++ remessa-producer/src/test/java/com/floorplan/remessaproducer/RemessaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.floorplan.remessaproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemessaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
